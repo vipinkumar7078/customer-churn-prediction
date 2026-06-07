4ec129081d645e4612e08a04961bb1ce9bee99f8
vipin kumar <vipin.rajput707805@gamil.com>
2026-06-07 12:17:03 +0530

    Added optimized model files

--- train_model.py
+++ train_model.py
@@ -8,10 +8,10 @@ from sklearn.metrics import accuracy_score
 # Load Dataset
 df = pd.read_csv("synthetic_customer_churn_100k.csv")
 
-# Column names clean
+# Clean Column Names
 df.columns = df.columns.str.lower().str.strip()
 
-# Convert churn column
+# Convert Churn Column
 if df["churn"].dtype == "object":
     df["churn"] = df["churn"].map({"No": 0, "Yes": 1})
 
@@ -27,12 +27,20 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+# Random Forest Model
+rf = RandomForestClassifier(
+    n_estimators=50,
+    max_depth=10,
+    random_state=42
+)
+
 # Train Model
-rf = RandomForestClassifier(random_state=42)
 rf.fit(X_train, y_train)
 
-# Accuracy
+# Prediction
 y_pred = rf.predict(X_test)
+
+# Accuracy
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
 # Save Model
@@ -40,3 +48,6 @@ joblib.dump(rf, "churn_model.pkl")
 joblib.dump(X.columns.tolist(), "model_columns.pkl")
 
 print("Model Saved Successfully!")
+
+
+
